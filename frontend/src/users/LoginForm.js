bbfde5bde71d5009a851062fafc85068ba5300e5
KenanTaylor <kenan.taylor99@gmail.com>
2023-10-29 15:01:20 -0400

    jwts

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -13,13 +13,32 @@ function LoginForm() {
         password: ''
     })
 
-    const [errorMessage, setErrorMessage] = useState(null)
+        const [errorMessage, setErrorMessage] = useState(null)}
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-       
 
+    ___
+     async function handleSubmit(e) {
+    const response = await fetch(`http://localhost:5000/authentication/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+
+    const data = await response.json()
+
+    if (response.status === 200) {
+        setCurrentUser(data.user)
+        console.log(data.token)
+        history.push(`/`)
+    } else {
+        setErrorMessage(data.message)
     }
+
+
+
+    
 
     return (
         <main>
@@ -65,4 +84,4 @@ function LoginForm() {
     )
 }
 
-export default LoginForm
+    export default LoginForm
